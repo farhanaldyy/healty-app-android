8eb65d942310bd2579687fccf44814a3728066f6
farhan aldiansyah <farhanpoetra011@gmail.com>
2021-04-21 01:09:49 +0700

    Membuat halaman login

--- app/src/main/java/com/example/healtyapp/MainActivity.kt
+++ app/src/main/java/com/example/healtyapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.healtyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -8,6 +9,7 @@ import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.add
+import com.example.healtyapp.activity.LoginActivity
 import com.example.healtyapp.fragment.AccountFragment
 import com.example.healtyapp.fragment.HomeFragment
 import com.example.healtyapp.fragment.PoliFragment
@@ -24,6 +26,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var menu: Menu
     private lateinit var menuItem: MenuItem
     private lateinit var bottomNavigationView: BottomNavigationView
+
+    private var statusLogin = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,7 +58,11 @@ class MainActivity : AppCompatActivity() {
                     callFragment(1, fragmentPoli)
                 }
                 R.id.navigation_account -> {
-                    callFragment(2, fragmentAccount)
+                    if(statusLogin) {
+                        callFragment(2, fragmentAccount)
+                    } else {
+                        startActivity(Intent(this, LoginActivity::class.java))
+                    }
                 }
             }
             false
---
+++ app/src/main/java/com/example/healtyapp/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.healtyapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.healtyapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
